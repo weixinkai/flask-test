029a52ddfcf3f4ee77b0bbf09ca0eabff5f4427b
k <k@example.com>
2016-07-21 17:18:47 +0800

    add search func

--- web.py
+++ web.py
@@ -28,17 +28,40 @@ def show_items(page=1):
     items = [dict(title=row[0], date=row[1], url=row[2])
              for row in cur.fetchall()]
     return render_template('show.html', items=items,
-                           current_page = page, max_page=max_page)
+                           current_page=page, max_page=max_page)
+
+@app.route('/search')
+@app.route('/search/<int:page>')
+def search(page=1):
+    condition = request.args.get('title', '')
+    order = get_order()
+    max_page = count_page(condition)
+    cur = g.db.execute('''SELECT DISTINCT * FROM collection WHERE title LIKE ?
+                          ORDER BY date '''+ order + ' LIMIT ? OFFSET ?',
+                          ('%'+condition+'%', ITEM_NUM_PER_PAGE, ITEM_NUM_PER_PAGE * (page - 1), ))
+    items = [dict(title=row[0], date=row[1], url=row[2])
+             for row in cur.fetchall()]
+    return render_template('search.html', items=items, condition=condition,
+                           current_page=page, max_page=max_page)
+
 
 @app.route('/goto',methods=['POST', 'GET'])
 def goto():
     if request.method == 'POST':
         page = request.form['target_page']
-        return redirect(url_for('show_items', page=page))
+        page_type = request.form['type']
+
+        if page_type == 'show':
+            return redirect(url_for('show_items', page=page))
+        elif page_type == 'search':
+            condition = request.args.get('title', '')
+            return redirect(url_for('search', page=page, title=condition))
+
     return redirect(url_for('index'))
 
-def count_page():
-    cur = g.db.execute('SELECT count(DISTINCT url) FROM collection')
+def count_page(condition=''):
+    cur = g.db.execute('SELECT count(DISTINCT url) FROM collection WHERE title LIKE ?',
+                       ('%'+condition+'%',))
     return ceil(cur.fetchone()[0] / ITEM_NUM_PER_PAGE)
 
 def get_order():
